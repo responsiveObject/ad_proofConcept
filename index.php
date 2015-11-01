66f7e4038ea6b933e896cef80be8907637e85f57
sgelinas <sgelinas@gmail.com>
2015-11-01 07:07:26 -0500

    get image in  base64

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
-<?php
-echo 'hello from index';
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+<div style="width: 1280px; height:720px">
+<?php
+
+echo 'create imagick image';
+
+try {
+    $im = new Imagick();
+    $im->newimage(200, 100, 'red', 'png');
+    //$im->writeimage('/var/www/media/test.png');
+    // start buffering
+//    ob_start();
+//    $thumbnail = $im->getImageBlob();
+//    $contents = ob_get_contents();
+//    ob_end_clean();
+//    echo "<img src='data:image/jpg;base64,".base64_encode($contents)."' />";
+    echo '<img src="data:image/jpg;base64,'.base64_encode($im->getImageBlob()).'" alt="" />';
+    $im->destroy();
+    
+} catch (Exception $ex) {
+    echo $e->getMessage();
+}
+
+?>
+
+    <img src="images/R24_BASE-wall_WRINKLE.png">
+</div>
